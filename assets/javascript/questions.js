016ff42b04551d939468e09615bd03e749cbb8bd
angela <angelagongli@gmail.com>
2020-01-19 23:39:07 -0600

    add .js files

---
+++ assets/javascript/questions.js
@@ -0,0 +1,45 @@
+var questions = [
+    {
+        title: "An array always begins at an index of:",
+        choices: ["-1", "0", "1", "null"],
+        answer: "0"
+    },
+    {
+        title: "A function within an object is called a ____.",
+        choices: ["property", "key", "value", "method"],
+        answer: "method"
+    },
+    {
+        title: "The difference between the == and === operators does not involve:",
+        choices: ["strict comparison", "assignment", "type conversion", "abstract comparison"],
+        answer: "assignment"
+    },
+    {
+        title: "Which of the following statements does not create a loop?",
+        choices: ["switch", "for", "while", "do...while"],
+        answer: "switch"
+    },
+    {
+        title: "Which of the following is not a way to declare a variable?",
+        choices: ["var", "set", "let", "const"],
+        answer: "set"
+    },
+    {
+        title: "Which of the following functions is not dependent on the user?",
+        choices: ["alert", "prompt", "console.log()", "confirm"],
+        answer: "console.log()"
+    },
+    {
+        title: "Which of the following facts about console.log() is false?",
+        choices: ["console.log() is a method", 
+                "console.log() accepts at least one parameter",
+                "console.log() is often used for debugging",
+                "console.log() displays output visible to the user on the page"],
+        answer: "console.log() displays output visible to the user on the page"
+    },
+    {
+        title: "Which of the following cannot be found in the DOM?",
+        choices: ["sibling nodes", "parent nodes", "tree nodes", "child nodes"],
+        answer: "tree nodes"
+    },
+];
